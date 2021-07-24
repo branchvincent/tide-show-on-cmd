4599b02673f3ebcf2e1d4229b6e98b3ee2e7e783
Branch Vincent <branchevincent@gmail.com>
2021-07-24 17:05:08 -0400

    support tide@v5

--- functions/_tide_item_kubectl.fish
+++
@@ -1,5 +0,0 @@
-function _tide_item_kubectl --description "Show Kubernetes context"
-    type -q kubectl || return
-    set -l context (kubectl config view --minify --output 'jsonpath={.current-context}/{..namespace}' 2>/dev/null) &&
-        echo (set_color magenta)⎈ (string replace --regex '/(|default)$' '' $context)
-end
